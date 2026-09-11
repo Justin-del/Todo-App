7cbb7378ebeaaabaf96afb12d97a5d6bffb8653c
Justin Chan Lok Hang <beibei031119@gmail.com>
2026-09-11 20:00:05 +0800

    Updated the appStartPath in capacitor.config.ts to "/todos.html".

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'Todo App',
-  webDir: 'build'
+  webDir: 'build',
+  server:{
+    appStartPath:"/todos.html"
+  },
 };
 
 export default config;
