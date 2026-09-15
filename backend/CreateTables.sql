dfa0a7cec1bc3d35ce0f24de782a51fee0f23c0c
Justin Chan Lok Hang <beibei031119@gmail.com>
2026-09-16 01:48:43 +0800

    Implemented a sql trigger for preventing immutable fields of the todo table from being updated once their values are set.

--- backend/CreateTables.sql
+++ backend/CreateTables.sql
@@ -4,7 +4,7 @@
 -- As the description can be optional, description can be an empty string.
 create table if not exists todo (id TEXT NOT NULL PRIMARY KEY, title TEXT NOT NULL, description TEXT NOT NULL, is_completed INTEGER NOT NULL, created_at TEXT NOT NULL DEFAULT (strftime('%Y-%m-%dT%H:%M:%SZ')), updated_at TEXT, user_id TEXT NOT NULL, FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE);
 
--- Create an UPDATE trigger on the todo table
+
 CREATE TRIGGER IF NOT EXISTS update_todo_updated_at
 AFTER UPDATE ON todo
 WHEN 
@@ -16,4 +16,14 @@ BEGIN
   UPDATE todo 
   SET updated_at = strftime('%Y-%m-%dT%H:%M:%SZ')
   WHERE id = NEW.id;
+END;
+
+CREATE TRIGGER IF NOT EXISTS prevent_todo_immutable_fields_from_being_updated_once_set
+BEFORE UPDATE ON todo
+WHEN
+  ((NEW.id IS NOT OLD.id) AND (OLD.id is NOT NULL)) OR
+  ((NEW.created_at IS NOT OLD.created_at) AND (OLD.created_at IS NOT NULL)) OR
+  ((NEW.user_id IS NOT OLD.user_id) AND (old.user_id IS NOT NULL))
+BEGIN
+  SELECT RAISE(ABORT,'id, created_at, and user_id of the todo table are immutable once their values are set.');
 END;
